f76902ebd249f68ef06e12647902a28f248e4466
Kevin Forte <87152919+Sk4rKr0w@users.noreply.github.com>
2024-02-13 01:23:50 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const hamburgerIcon = document.getElementById('hamburger-icon');
-    const sidebar = document.querySelector('.sidebar');
-
-    hamburgerIcon.addEventListener('click', function () {
-        sidebar.style.left = (sidebar.style.left === '-15%' || sidebar.style.left === '') ? '0px' : '-15%';
-        hamburgerIcon.classList.toggle('active');
-    });
-});
